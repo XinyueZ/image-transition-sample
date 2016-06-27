714532407481f5a90f6ce5a0986599cf663a0485
Chris Zhao <xinyue.zhao@immonet.de>
2016-06-27 13:35:22 +0200

    Add share for detail, add time on detail

--- app/src/main/java/com/demo/transition/image/app/activities/DetailActivity.java
+++ app/src/main/java/com/demo/transition/image/app/activities/DetailActivity.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.demo.transition.image.R;
@@ -19,6 +23,7 @@ import static android.os.Bundle.EMPTY;
 public final class DetailActivity extends  BaseActivity {
 	private static final String EXTRAS_IMAGE = DetailActivity.class.getName() + ".EXTRAS.image";
 	private static final int LAYOUT = R.layout.activity_detail;
+	private static final int MENU_DETAIL = R.menu.menu_detail;
 	private ActivityDetailBinding mBinding;
 
 	static void showInstance(Activity cxt, Image image) {
@@ -46,4 +51,22 @@ public final class DetailActivity extends  BaseActivity {
 		setTitle(image.getTitle());
 		mBinding.setImage(image);
 	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(MENU_DETAIL, menu);
+		MenuItem item = menu.findItem(R.id.menu_item_share);
+		ShareActionProvider myShareActionProvider =
+				(ShareActionProvider) MenuItemCompat.getActionProvider(item);
+		Intent myShareIntent = new Intent(Intent.ACTION_SEND);
+		myShareIntent.setType("text/plain");
+		Serializable imageMeta = getIntent().getSerializableExtra(EXTRAS_IMAGE);
+		Image image = (Image) imageMeta;
+		myShareIntent.putExtra(Intent.EXTRA_TEXT, String.format("%s",image.getImageUrl().getHd()));
+		myShareActionProvider.setShareIntent(myShareIntent);
+
+		return true;
+	}
+
 }
