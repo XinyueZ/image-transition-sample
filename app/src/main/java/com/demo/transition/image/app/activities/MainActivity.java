03c5713e73fa0b2d8b987a5a4eb767c1add34e89
Chris Zhao <xinyue.zhao@immonet.de>
2016-06-30 12:49:49 +0200

    Reset "onBackPressed" in main-activity

--- app/src/main/java/com/demo/transition/image/app/activities/MainActivity.java
+++ app/src/main/java/com/demo/transition/image/app/activities/MainActivity.java
@@ -14,9 +14,11 @@ import com.demo.transition.image.app.App;
 import com.demo.transition.image.app.fragments.DetailFragment;
 import com.demo.transition.image.app.fragments.MainFragment;
 import com.demo.transition.image.bus.ClickImageEvent;
+import com.demo.transition.image.bus.CloseDetailFragmentEvent;
 import com.demo.transition.image.bus.PopUpDetailFragmentEvent;
 import com.demo.transition.image.transition.v21.PlatformTransition;
 
+import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 
 import static com.demo.transition.image.app.App.KEY_OPEN_DETAIL_ACTIVITY;
@@ -63,8 +65,7 @@ public final class MainActivity extends BaseActivity {
 				if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
 					DetailActivity.showInstance(this, e.getImage(), e.getThumbnail());
 				} else {
-					String transitionName = ViewCompat.getTransitionName(e.getSharedImageView());
-					ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, e.getSharedImageView(), "shareName");
+					ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, e.getSharedImageView(), App.Instance.getString(R.string.default_transition_name_detail));
 					DetailActivity.showInstance(this, e.getImage(), e.getThumbnail(), options);
 				}
 			} else {
@@ -106,19 +107,19 @@ public final class MainActivity extends BaseActivity {
 	}
 
 
-//	@Override
-//	public void onBackPressed() {
-//		boolean currentOpenDetailActivity = App.Instance.getSharedPreferences(PREFS, Context.MODE_PRIVATE)
-//		                                                .getBoolean(KEY_OPEN_DETAIL_ACTIVITY, App.DEFAULT_OPEN_DETAIL_ACTIVITY);
-//		if (!currentOpenDetailActivity) {
-//			if (getSupportFragmentManager().getBackStackEntryCount() == 0) {
-//				super.onBackPressed();
-//			} else {
-//				EventBus.getDefault()
-//				        .post(new CloseDetailFragmentEvent());
-//			}
-//		} else {
-//			super.onBackPressed();
-//		}
-//	}
+	@Override
+	public void onBackPressed() {
+		boolean currentOpenDetailActivity = App.Instance.getSharedPreferences(PREFS, Context.MODE_PRIVATE)
+		                                                .getBoolean(KEY_OPEN_DETAIL_ACTIVITY, App.DEFAULT_OPEN_DETAIL_ACTIVITY);
+		if (!currentOpenDetailActivity) {
+			if (getSupportFragmentManager().getBackStackEntryCount() == 0) {
+				super.onBackPressed();
+			} else {
+				EventBus.getDefault()
+				        .post(new CloseDetailFragmentEvent());
+			}
+		} else {
+			super.onBackPressed();
+		}
+	}
 }
